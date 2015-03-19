664cafcf28b59e4b828f83dd73b2c247af9bd569
sutirthchakravarty <sutirth.chakravarty@gmail.com>
2015-03-19 16:58:38 +0530

    Second activity

---
+++ AndroidStudioProjects/MyApplication2/app/src/main/java/helloworld/example/com/myapplication/SecondActivity.java
@@ -0,0 +1,17 @@
+package helloworld.example.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by sutirthchakravarty on 3/19/15.
+ */
+public class SecondActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState){
+      super.onCreate(savedInstanceState);
+
+
+    }
+}
