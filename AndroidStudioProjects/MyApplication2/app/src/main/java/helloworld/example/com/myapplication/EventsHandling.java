8b9638081ccab4f9575da893f1f128142d2ebd52
sutirthchakravarty <sutirth.chakravarty@gmail.com>
2015-03-19 17:39:19 +0530

    Event Handling- adding strings as well

--- AndroidStudioProjects/MyApplication2/app/src/main/java/helloworld/example/com/myapplication/EventsHandling.java
+++ AndroidStudioProjects/MyApplication2/app/src/main/java/helloworld/example/com/myapplication/EventsHandling.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
  * Created by sutirthchakravarty on 3/19/15.
  */
 public class EventsHandling extends Activity {
-
+   String strHello = null;
   protected void onCreate(Bundle savedInstanceState){
       super.onCreate(savedInstanceState);
   }
