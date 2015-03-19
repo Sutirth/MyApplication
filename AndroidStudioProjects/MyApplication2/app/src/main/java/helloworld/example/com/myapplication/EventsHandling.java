7255b958b9310367d080e35e2fb3b1002ad606fa
sutirthchakravarty <sutirth.chakravarty@gmail.com>
2015-03-19 17:26:25 +0530

    Event Handling

---
+++ AndroidStudioProjects/MyApplication2/app/src/main/java/helloworld/example/com/myapplication/EventsHandling.java
@@ -0,0 +1,14 @@
+package helloworld.example.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sutirthchakravarty on 3/19/15.
+ */
+public class EventsHandling extends Activity {
+
+  protected void onCreate(Bundle savedInstanceState){
+      super.onCreate(savedInstanceState);
+  }
+}
